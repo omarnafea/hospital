4477d8a82b857f4a708e30ce8a3c20a17e8fa657
omar <onafea08@gmail.com>
2021-08-03 10:56:36 +0300

    edit

--- index.php
+++ index.php
@@ -44,17 +44,21 @@ $tests = $statement->fetchAll();
 	  <div class="carousel-inner">
 
           <h6>Appointments WEB SOLUTIONS</h6>
-          <h3>Your Partner in Growth</h3>
-          <p>You have goals, you need results, we have the way to get you there. We create digital solutions that will convert your visitors into real clients.</p>
+          <h3>Welcome to my simple project</h3>
+          <p>To solve the problem of appointments inside the hospital inside the south, especially the city of Aqaba, as Hashem Hospital serves about six villages around Aqaba, so this site was designed to help this category who comes from far places and also serves the category of special needs that are difficult to reach the hospital to book an appointment.</p>
           <a href="appointment" class="btn btn-light">Appointment Now</a>
           <div class="overlay"></div>
 
           <div class="carousel-item active">
-              <img src="uploads/slider1.jpg" class="d-block w-100" alt="Image" >
+              <img src="uploads/ThinkstockPhotos-842852832.jpg" class="d-block w-100" alt="Image" >
           </div>
 
           <div class="carousel-item">
-              <img src="uploads/slider2.jpg" class="d-block w-100" alt="Image" >
+              <img src="uploads/medical-appointment-reminder-people.jpg" class="d-block w-100" alt="Image" >
+          </div>
+          
+          <div class="carousel-item">
+              <img src="uploads/MedicalAppointment.dafc7db6805b5541eb843f4870979680500.jpg" class="d-block w-100" alt="Image" >
           </div>
       </div>
 	</div>
@@ -63,13 +67,7 @@ $tests = $statement->fetchAll();
 
 
 <!--start who we are-->
-<div class="who-we-are">
-	<div class="container">
-		<h6 class="text-center">WHO WE ARE</h6>
-		<p class="text-center" id="about_text">Who we are here</p>
 
-	</div>
-</div>
 <!--end hwo we are-->
 
 
@@ -85,13 +83,11 @@ $tests = $statement->fetchAll();
 				<div class="helpful-links">
 					<h2>Contact Us</h2>
 					<ul class="list-unstyled">
-						<Li id="">address: Jordan / amman </Li>
+						
+						<Li id="">address: Jordan / aqaqba </Li>
 						<Li id=""> Phone: 0797243170 </Li>
 
-						<Li id="">address2: Jordan / amman </Li>
-						<Li id=""> Phone: 0797243170 </Li>
-
-						<Li id="">Email:<a href="mailto:info@appointment.com?subject=contact">info@appointment.com</a> </Li>
+						<Li id="">Email:<a href="mailto:info@appointment.com?subject=contact">abasithaer8@gmail.com</a> </Li>
 					</ul>
 					
 				</div>
